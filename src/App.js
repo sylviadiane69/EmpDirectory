b5871a690c9dd89da0148fbfc1f28ac4de95d7f5
Sylvia Soto <sylviadiane69@gmail.com>
2020-06-07 16:48:46 -0500

    updated files to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Discover from "./pages/Discover";
-import About from "./pages/About";
-import Search from "./pages/Search";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import SearchForm from "./components/SearchForm";
+import Table from "./components/Table";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <Header />
+      <SearchForm />
+      <Table />
+    </div>
   );
 }
 
---
+++ src/components/Table.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class Table extends Component {
+   constructor(props) {
+      super(props) 
+      this.state = { 
+         employees: [
+            { id: 1, name: 'Gloria Ramirez', title: 'CEO', salary: '500,000'},
+            { id: 2, name: 'Gabriel Cervantes', title: 'Engineer', salary: '160,000' },
+            { id: 3, name: 'Michelle Holub', title: 'Devloper', salary: '125,000' },
+            { id: 4, name: 'Richard Allen', title: 'HR Manager', salary: '100,000' },
+            { id: 5, name: 'Leslie Perez', title: 'Supervisor', salary: '175,000' },
+            { id: 6, name: 'Matthew Scott', title: 'Engineer', salary: '165,000' },
+            { id: 7, name: 'Judy Sanchez', title: 'Devloper', salary: '125,000' },
+            { id: 8, name: 'Christopher James', title: 'Intern', salary: '55,000' }
+         ]
+      }
+   }
+
+   render() { 
+      return (
+         <div>
+            <h1>Employee Table</h1>
+         </div>
+      )
+   }
+}
+
+export default Table
