eb37ea1a8d14140f608c5811c00a949dc48ba44f
Sylvia Soto <sylviadiane69@gmail.com>
2020-06-08 21:56:39 -0500

    changes to files

--- src/App.js
+++ src/App.js
@@ -1,44 +1,7 @@
-// import React from "react";
-// import "../styles/App.css";
-
-// Const App = () => {
-//   employees: [
-//     { id: 1, name: 'Gloria Ramirez', title: 'CEO', salary: '500,000'},
-//     { id: 2, name: 'Gabriel Cervantes', title: 'Engineer', salary: '160,000' },
-//     { id: 3, name: 'Michelle Holub', title: 'Devloper', salary: '125,000' },
-//     { id: 4, name: 'Richard Allen', title: 'HR Manager', salary: '100,000' },
-//     { id: 5, name: 'Leslie Perez', title: 'Supervisor', salary: '175,000' },
-//     { id: 6, name: 'Matthew Scott', title: 'Engineer', salary: '165,000' },
-//     { id: 7, name: 'Judy Sanchez', title: 'Devloper', salary: '125,000' },
-//     { id: 8, name: 'Christopher James', title: 'Intern', salary: '55,000' }
-//  ]
-
-//  const renderEmployees = (player, index) => {
-//    return(
-//      <tr key={index}>
-//        <td>{employee.id}</td>
-//        <td>{employee.name}</td>
-//        <td>{employee.title}</td>
-//        <td>{employee.salary}</td>
-//      </tr>
-//    )
-//  }
-
-//  return (
-//    <div className="App"></div>
-
-//  )
-
-
-
-  
-// }
-
-
 import React from "react";
 import Header from "./components/Header";
 import SearchForm from "./components/SearchForm";
-// import Table from "./components/Table";
+import Table from "./components/Table";
 import Data from "./components/Data"
 
 function App() {
@@ -46,7 +9,7 @@ function App() {
     <div>
       <Header />
       <SearchForm />
-      {/* <Table /> */}
+      <Table />
       <Data />
     </div>
   );
