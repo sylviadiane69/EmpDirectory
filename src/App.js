e50ad22b4d99853ced2f3c54a268aaf60a46f3cc
Sylvia Soto <sylviadiane69@gmail.com>
2020-06-12 09:29:45 -0500

    added a footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import SearchForm from "./components/SearchForm";
 import Table from "./components/Table";
+import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Wrapper>
         <Table />
       </Wrapper>
+      <Footer />
     </div>
   );
 }
